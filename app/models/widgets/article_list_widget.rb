15a2c0da06a13bd7d299f7e5aea418b4486502cc
David Gómez <david.gomez@marsbased.com>
2015-05-13 19:03:46 +0200

    store_accesor modified

--- app/models/widgets/article_list_widget.rb
+++ app/models/widgets/article_list_widget.rb
@@ -1,7 +1,7 @@
 class ArticleListWidget < Widget
   self.widget_options = ['max']
 
-  store_accessor :options, :max
+  store_accessor :options, *widget_options
 
   def articles
     Article.all.limit(max)
